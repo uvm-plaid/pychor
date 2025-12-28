adcd7a0b160fa15acc199122ed82874cf95645ed
Joe Near <jnear@uvm.edu>
2025-12-28 13:57:11 -0500

    update OT

--- examples/protocol_ot.py
+++ examples/protocol_ot.py
@@ -1,23 +1,32 @@
 import pychor
-from nacl.public import PrivateKey, Box, SealedBox
+from nacl.public import PrivateKey, PublicKey, Box, SealedBox
+from nacl.utils import random
 import galois
 
 GF_2 = galois.GF(2)
 
 def ot(sender, receiver, select_bits, options):
+    @pychor.local_function
     def gen_keys(select_bits):
         s1, s2 = select_bits
-        key_pairs = [PrivateKey.generate() for _ in range(4)]
+        # Generate 4 random public keys (for which there is no secret key)
+        public_keys = [PublicKey(random(PublicKey.SIZE)) for _ in range(4)]
+        # Generate 1 actual keypair
+        key_pair = PrivateKey.generate()
+        # Replace the selected row's (random) public key with the actual public key
         row_num = int(s1)*2 + int(s2)
-        saved_key = key_pairs[row_num]
-        return row_num, saved_key, [k.public_key for k in key_pairs]
+        public_keys[row_num] = key_pair.public_key
 
+        return row_num, key_pair, public_keys
+
+    @pychor.local_function
     def encrypt_options(pub_keys, options):
         options_bytes = [int(x).to_bytes(1, 'little') for x in options]
         encrypted_options = [SealedBox(pk).encrypt(x) for pk, x in \
                              zip(pub_keys, options_bytes)]
         return encrypted_options
 
+    @pychor.local_function
     def decrypt_result(row_num, saved_key, encrypted_options):
         selected_option = encrypted_options[row_num]
         plaintext = SealedBox(saved_key).decrypt(selected_option)
@@ -28,19 +37,19 @@ def ot(sender, receiver, select_bits, options):
     # ==================================================
 
     # generate keys
-    row_num, saved_key, pub_keys = pychor.locally(gen_keys, select_bits).untup(3)
+    row_num, saved_key, pub_keys = gen_keys(select_bits).untup(3)
 
     # send public keys to sender
-    pub_keys.send(sender, receiver, note='public keys')
+    pub_keys.send(receiver, sender, note='public keys')
 
     # encrypt the options
-    encrypted_options = pychor.locally(encrypt_options, pub_keys, options)
+    encrypted_options = encrypt_options(pub_keys, options)
 
     # send them to the receiver
     encrypted_options.send(sender, receiver, note='encrypted values')
 
     # decrypt the result
-    result = pychor.locally(decrypt_result, row_num, saved_key, encrypted_options)
+    result = decrypt_result(row_num, saved_key, encrypted_options)
 
     return result
 
